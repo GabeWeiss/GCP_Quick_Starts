ef601f2b1ae6b359a19699b49e851899feaf7c25
Gabe Weiss <GabeWeiss@users.noreply.github.com>
2019-02-08 16:59:09 -0800

    Fix json auth with the service account

--- 03_basics_gcf/main.py
+++ 03_basics_gcf/main.py
@@ -1,6 +1,7 @@
 import base64
 import logging
 import os
+import sys
 
 from google.oauth2 import service_account
 from googleapiclient import discovery
@@ -15,17 +16,26 @@ from google.auth import compute_engine
 #   permission levels, etc. For the purposes of this blog demo code, I've
 #   done it for ease of calling and to just show what the data looks like.
 #   Please please don't do this in production.
-serviceAccount = '<paste contents of the service account json blob here>'
+service_account_json = <paste contents of the service account json blob here> # Should be a json blob
 
+# You shouldn't need to change this variable, it's here for illustration
+# purposes. An advantage of calling a Cloud Function within the same project
+# is that there are certain environment varibales you get for free like this
+# one
 project_id  = os.environ['GCP_PROJECT']
-gcp_region  = 'us-central1'
-registry_id = 'iot_core_demo'
-device_id   = request_json['deviceId']
+
+# change the following to match your project's values
+# you could also easily modify the code to receieve these as variables
+# in the GET call since I'm relying on that for the config/command switch
+# as well as the actual message being sent
+registry_id = '<registry_id>'
+gcp_region  = '<project location>'
+device_id   = '<device_id>'
 
 # a couple default values for the config messages which will
 # reset the LED configurations as a default
 incoming_msg   = "clear"  # by default will reset the LED matrix
-which_msg_type = "config" # by default will send a config message
+incoming_which = "config" # by default will send a config message
 
 # I'm putting these two variables outside the method only because there may
 # be a time that there are multiple versions and rest APIs for IoT Core, and
@@ -34,15 +44,14 @@ which_msg_type = "config" # by default will send a config message
 api_version = 'v1'
 service_name = 'cloudiotcore'
 
-def get_client(service_account_json):
+def get_client():
 
 #   Returns an authorized API client by discovering the IoT API and creating
 #   a service object using the service account credentials JSON.
     api_scopes = ['https://www.googleapis.com/auth/cloud-platform']
     discovery_api = 'https://cloudiot.googleapis.com/$discovery/rest'
 
-    import pdb;pdb.set_trace()
-    credentials = compute_engine.Credentials.from_service_account_file(service_account_json)
+    credentials = service_account.Credentials.from_service_account_file(service_account_json)
     scoped_credentials = credentials.with_scopes(api_scopes)
 
     discovery_url = '{}?version={}'.format(discovery_api, api_version)
@@ -54,22 +63,28 @@ def get_client(service_account_json):
             credentials=scoped_credentials)
 
 def update_device(request):
-    #request_json = request.get_json(force=True)
-    #logging.info(request_json)
-    #if not request_json:
-    #    return 'We need more details!'
+    # If the user passed in a message variable, then update our
+    # incoming message to match
+    new_config = request.args.get('message')
+    final_msg = incoming_msg
+    if new_config:
+      final_msg = new_config
 
-      # TODO: Need to change the new_config over to fetching the GET parameter and
-      # changing the msg variable to whatever they sent
-    new_config = request.GET.get('message')
-    binary_data = base64.urlsafe_b64encode(incoming_msg.encode('utf-8')).decode('ascii')
+    binary_data = base64.urlsafe_b64encode(final_msg.encode('utf-8')).decode('ascii')
     device_name = 'projects/{}/locations/{}/registries/{}/devices/{}'.format(project_id,
                                                                              gcp_region,
                                                                              registry_id,
                                                                              device_id)
 
-    client = get_client(service_account)
-    if which_msg_type == "command":
+    # If the user passed a which variable, then update config v. command
+    new_which = request.args.get('which')
+    final_which = incoming_which
+    if new_which:
+      final_which = new_which
+
+    client = get_client()
+
+    if final_which == "command":
       logging.info("Sending a command")
       command_request = {
         "name": device_name,
@@ -79,7 +94,7 @@ def update_device(request):
       return client.projects().locations().registries().devices().sendCommandToDevice(
                                                           name=device_name,
                                                           body=command_request).execute()
-    elif which_msg_type == "config":
+    elif final_which == "config":
       config_request = {
         "name": device_name,
         "binaryData": binary_data
@@ -88,4 +103,4 @@ def update_device(request):
       return client.projects().locations().registries().devices().modifyCloudToDeviceConfig(
                                                           name=device_name,
                                                           body=config_request).execute()
-    return 0
+    return '500'
