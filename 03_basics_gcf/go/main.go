3d8ea0e5467b92df274eea00fb7d5a90f38335b6
Gabe Weiss <GabeWeiss@users.noreply.github.com>
2019-03-11 16:49:49 -0700

    First version of go tutorial...it configs things! Still not king. Still not ready for deployment into blog, but closer.

---
+++ 03_basics_gcf/go/main.go
@@ -0,0 +1,72 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"context"
+	b64 "encoding/base64"
+	"fmt"
+
+	"golang.org/x/oauth2/google"
+	cloudiot "google.golang.org/api/cloudiot/v1"
+)
+
+// getClient returns a client based on the environment variable GOOGLE_APPLICATION_CREDENTIALS
+func getClient() (*cloudiot.Service, error) {
+	// Authorize the client using Application Default Credentials.
+	// See https://g.co/dv/identity/protocols/application-default-credentials
+	ctx := context.Background()
+	httpClient, err := google.DefaultClient(ctx, cloudiot.CloudPlatformScope)
+	if err != nil {
+		return nil, err
+	}
+	client, err := cloudiot.New(httpClient)
+	if err != nil {
+		return nil, err
+	}
+
+	return client, nil
+}
+
+func setConfig(client *cloudiot.Service, projectID string, region string, registryID string, deviceID string, configData string) (*cloudiot.DeviceConfig, error) {
+	req := cloudiot.ModifyCloudToDeviceConfigRequest{
+		BinaryData: b64.StdEncoding.EncodeToString([]byte(configData)),
+	}
+
+	path := fmt.Sprintf("projects/%s/locations/%s/registries/%s/devices/%s", projectID, region, registryID, deviceID)
+	response, err := client.Projects.Locations.Registries.Devices.ModifyCloudToDeviceConfig(path, &req).Do()
+	if err != nil {
+		return nil, err
+	}
+
+	return response, nil
+}
+
+func main() {
+
+	client, clientErr := getClient()
+
+	if clientErr != nil {
+		fmt.Println("Failed to get auth'd client")
+	}
+
+	_, configErr := setConfig(client, "gweiss-simple-path", "us-central1", "gweiss-simple-00", "gweiss-arduino-00", "clear")
+	if configErr != nil {
+		fmt.Println("Failed to configure something")
+		fmt.Printf("%s", configErr)
+	}
+
+	fmt.Println("Hello world")
+}
