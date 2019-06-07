7fcd92ca1981e396c475b7fd1de1f296602b9ae1
Gabe Weiss <GabeWeiss@users.noreply.github.com>
2019-06-07 16:45:25 -0700

    Re-organize a bit to make it easier to edit variables to change some parameters

--- storage/01_basics.py
+++ storage/01_basics.py
@@ -6,16 +6,19 @@ from google.cloud import storage
 import time
 
 image_dir = "images"
+cur_time = time.time()
+filename = "image_{}.jpg".format(cur_time)
+
+image_size = "1280x720"
+
+bucket_name = "blog_pi_images"
+
 if not os.path.exists(image_dir):
     os.makedirs("images")
 
-cur_time = time.time()
-filename = "image_{}.jpg".format(cur_time)
-subprocess.run(["fswebcam", "-r 1280x720", "--no-banner", "{}/{}".format(image_dir, filename)])
+subprocess.run(["fswebcam", "-r {}".format(image_size), "--no-banner", "{}/{}".format(image_dir, filename)])
 
 storage_client = storage.Client()
-bucket = storage_client.get_bucket("blog_pi_images")
+bucket = storage_client.get_bucket(bucket_name)
 blob = bucket.blob(filename)
-blob.upload_from_filename("./images/{}".format(filename))
-
-
+blob.upload_from_filename("./{}/{}".format(image_dir, filename))
